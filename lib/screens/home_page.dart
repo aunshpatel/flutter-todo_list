587fffcd4777fcce4bb9bfe1cd76a5b7587737bf
aunshpatel <patelaunsh28@gmail.com>
2025-03-23 01:07:14 -0400

    Code cleanup

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -13,48 +13,48 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return PopScope(
-        canPop: false,
-        child: Scaffold(
-            endDrawerEnableOpenDragGesture: false,
-            drawer: const SideDrawer(),
-            appBar: AppBar(
-              leading: Builder(
-                builder: (BuildContext context) {
-                  return IconButton(
-                    icon: const Icon(Icons.menu),
-                    color: kWhiteColor,
-                    onPressed: () {
-                      Scaffold.of(context).openDrawer();
-                    },
-                    tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-                  );
+      canPop: false,
+      child: Scaffold(
+        endDrawerEnableOpenDragGesture: false,
+        drawer: const SideDrawer(),
+        appBar: AppBar(
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                icon: const Icon(Icons.menu),
+                color: kWhiteColor,
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
                 },
-              ),
-              actions: <Widget>[],
-              centerTitle: true,
-              title: Text('Home Screen', style:TextStyle(color: kWhiteColor),),
-              backgroundColor: kThemeBlueColor,
+                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+              );
+            },
+          ),
+          actions: <Widget>[],
+          centerTitle: true,
+          title: Text('Home Screen', style:TextStyle(color: kWhiteColor),),
+          backgroundColor: kThemeBlueColor,
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Column(
+                    children: [
+                      Text("Welcome to", style: kHomePageTitle),
+                      Text("The Todo List", style: kHomePageTitle),
+                    ],
+                  ),
+                )
+              ],
             ),
-            body: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(24),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Center(
-                      child: Column(
-                        children: [
-                          Text("Welcome to", style: kHomePageTitle),
-                          Text("The Todo List", style: kHomePageTitle),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            )
+          ),
         )
+      )
     );
   }
 }
