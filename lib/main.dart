46dc2c5f1a868cf8a0a99ad47a586ed69df7fff2
aunshpatel <patelaunsh28@gmail.com>
2025-03-23 00:03:51 -0400

    Add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_list/screens/home_page.dart';
+import 'package:todo_list/screens/widgets/consts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+ /* Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'The Todo List',
@@ -20,6 +22,29 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/home_page',
       routes: {
+        '/home_page':(context) => const HomePage(),
+      },
+    );
+  }*/
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'The Todo List',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(0, 227, 231, 239)),
+        useMaterial3: false,
+      ),
+      initialRoute: '/splash_screen',
+      routes: {
+        '/splash_screen': (context) => AnimatedSplashScreen(
+          duration: 3000,
+          centered: true,
+          splashIconSize: 230,
+          splash: 'assets/images/todo-list-white-transparent-bg.png',
+          nextScreen: const HomePage(),
+          splashTransition: SplashTransition.fadeTransition,
+          backgroundColor: kThemeBlueColor,
+        ),
         '/home_page':(context) => const HomePage(),
       },
     );
