acb0ce55fac366a27bb4a0baede31e7d2405666a
aunshpatel <patelaunsh28@gmail.com>
2025-03-26 11:52:33 -0400

    Fix code for login functionality

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_list/screens/widgets/consts.dart';
 import 'package:todo_list/screens/widgets/rounded_buttons.dart';
 import 'package:todo_list/screens/widgets/side_drawer.dart';
-
 import '../blocs/login/login_bloc.dart';
 import '../blocs/login/login_event.dart';
 import '../blocs/login/login_state.dart';
@@ -16,11 +15,9 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  String email = '', pwd = '';
-  String loginEmailID = '', loginPassword = '';
-  bool showSpinner = false, _passwordVisible = false;
+  bool showSpinner = false;
+  bool _passwordVisible = false;
 
-  // Login success or failure message
   void _showMessage(String title, String message, bool success) {
     showDialog(
       context: context,
@@ -47,152 +44,106 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      canPop: false,
-      child: Scaffold(
-        drawerEnableOpenDragGesture: true,
-        drawer: const SideDrawer(),
-        appBar: AppBar(
-          leading: Builder(
-            builder: (BuildContext context) {
-              return IconButton(
-                icon: const Icon(Icons.menu),
-                color: kWhiteColor,
-                onPressed: () {
-                  Scaffold.of(context).openDrawer();
-                },
-                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-              );
-            },
-          ),
-          actions: <Widget>[],
-          centerTitle: true,
-          title: Text('Login Screen', style:TextStyle(color: kWhiteColor),),
-          backgroundColor: kThemeBlueColor,
+    return Scaffold(
+      drawerEnableOpenDragGesture: true,
+      drawer: const SideDrawer(),
+      appBar: AppBar(
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              color: kWhiteColor,
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          },
         ),
-        body: Padding(
-          padding: const EdgeInsets.only(left:24.0,right:24.0),
-          child: showSpinner == true ? SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CircularProgressIndicator(),
-                SizedBox(height: 10,),
-                Text('Logging In'),
-              ],
+        centerTitle: true,
+        title: Text('Login Screen', style: TextStyle(color: kWhiteColor)),
+        backgroundColor: kThemeBlueColor,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 15.0),
+            Flexible(
+              child: HeroLogo(
+                height: 250,
+                image: 'assets/images/todo-list-dark-theme-logo.png',
+                tag: 'photo',
+              ),
             ),
-          ) : Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const SizedBox(
-                height: 15.0,
-              ),
-              Flexible(
-                child: HeroLogo(height:250,image:'assets/images/todo-list-dark-theme-logo.png', tag:'photo'),
-              ),
-              const SizedBox(
-                height: 15.0,
-              ),
-              TextField(
-                controller: emailController,
-                keyboardType: TextInputType.emailAddress,
-                onChanged:(value){
-                  email = emailController.text;
-                  // SharedPreferences.getInstance().then((prefs) {
-                  //   prefs.setString('email', email);
-                  // },);
+            SizedBox(height: 15.0),
+            TextField(
+              controller: emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: textInputDecoration('Enter your email'),
+            ),
+            SizedBox(height: 15.0),
+            TextField(
+              controller: passwordController,
+              obscureText: !_passwordVisible,
+              decoration: passwordInputDecoration(
+                'Enter your password',
+                _passwordVisible,
+                    () {
+                  setState(() {
+                    _passwordVisible = !_passwordVisible;
+                  });
                 },
-                decoration: textInputDecoration('Enter your email',),
               ),
-              // TextField(
-              //   controller: emailController,
-              //   decoration: const InputDecoration(labelText: 'Email'),
-              // ),
-              const SizedBox(
-                height: 15.0,
-              ),
-
-              TextField(
-                controller: passwordController,
-                obscureText: _passwordVisible == false ? true : false,
-                onChanged:(value){
-                  pwd = passwordController.text;
-                  // SharedPreferences.getInstance().then((prefs) {
-                  //   prefs.setString('password', pwd);
-                  // },);
-                },
-                decoration: passwordInputDecoration(
-                    'Enter your password',
-                    _passwordVisible,
-                        (){
-                      setState(() {
-                        _passwordVisible = !_passwordVisible;
-                      });
-                    }
-                ),
-              ),
-              // TextField(
-              //   controller: passwordController,
-              //   decoration: const InputDecoration(labelText: 'Password'),
-              //   obscureText: true,
-              // ),
-              const SizedBox(height: 20),
-              BlocListener<LoginBloc, LoginState>(
-                listener: (context, state) {
-                  if (state is LoginSuccess) {
-                    _showMessage('SUCCESS!', 'Congratulations, you have logged in successfully! You will be redirected to home page now.', true);
-                    setState(() {
-                      isLoggedIn = true;
-                      showSpinner = false;
-                    });
-                    // Navigator.pushReplacementNamed(context, '/home_page');
-                  } else if (state is LoginFailure) {
-                    setState(() {
-                      isLoggedIn = false;
-                      showSpinner = false;
-                    });
+            ),
+            SizedBox(height: 20),
+            BlocListener<LoginBloc, LoginState>(
+              listener: (context, state) {
+                if (state is LoginSuccess) {
+                  setState(() {
+                    showSpinner = false;
+                    isLoggedIn = true;
+                  });
+                  prefs.setBool('isLoggedIn', isLoggedIn);
+                  Future.delayed(Duration(milliseconds: 100), () {
+                    _showMessage('SUCCESS!', 'You have logged in successfully! Redirecting to home page.', true);
+                  });
+                } else if (state is LoginFailure) {
+                  setState(() {
+                    showSpinner = false;
+                    isLoggedIn = false;
+                  });
+                  prefs.setBool('isLoggedIn', isLoggedIn);
+                  Future.delayed(Duration(milliseconds: 100), () {
                     _showMessage('WARNING!', 'Login failed, please try again.', false);
-                  }
+                  });
+                }
+              },
+              child: BlocBuilder<LoginBloc, LoginState>(
+                builder: (context, state) {
+                  return showSpinner ? Center(child: CircularProgressIndicator())
+                  : RoundedButton(
+                    colour: kThemeBlueColor,
+                    title: 'Login',
+                    onPress: () {
+                      final email = emailController.text;
+                      final password = passwordController.text;
+                      setState(() {
+                        showSpinner = true;
+                      });
+                      context.read<LoginBloc>().add(
+                        LoginSubmitted(email: email, password: password),
+                      );
+                    },
+                  );
                 },
-                child: BlocBuilder<LoginBloc, LoginState>(
-                  builder: (context, state) {
-                    if (state is LoginLoading) {
-                      return const CircularProgressIndicator();
-                    }
-                    return RoundedButton(
-                      colour:kThemeBlueColor,
-                      title:'Login',
-                      onPress:()  {
-                        final email = emailController.text;
-                        final password = passwordController.text;
-                        setState(() {
-                          showSpinner = true;
-                        });
-                        context.read<LoginBloc>().add(LoginSubmitted(email: email, password: password));
-                      },
-                    );
-                    return ElevatedButton(
-                      onPressed: () {
-                        final email = emailController.text;
-                        final password = passwordController.text;
-                        setState(() {
-                          showSpinner = true;
-                        });
-                        context.read<LoginBloc>().add(LoginSubmitted(email: email, password: password));
-                      },
-                      child: const Text('Login'),
-                    );
-                  },
-                ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      )
+      ),
     );
   }
 }
