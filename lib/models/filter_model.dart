993e6500d37d552548775028a61fc8b661bf995d
aunshpatel <patelaunsh28@gmail.com>
2025-03-30 02:47:30 -0400

    Add filter model for the todos

---
+++ lib/models/filter_model.dart
@@ -0,0 +1,15 @@
+class TodoFilters {
+  String status;
+  String priority;
+  String startDateOrder;
+  String statusOrder;
+  String priorityOrder;
+
+  TodoFilters({
+    this.status = "",
+    this.priority = "",
+    this.startDateOrder = "",
+    this.statusOrder = "",
+    this.priorityOrder = "",
+  });
+}
